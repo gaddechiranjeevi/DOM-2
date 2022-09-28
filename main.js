6c78df646da2d2dab08ebf105dc41faadc98be30
gaddechiranjeevi <gaddechiru@gmail.com>
2022-09-28 13:25:08 +0530

    Element Access by Tag

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 
 var items= document.getElementsByClassName('list-group-item');
-items[2].style.backgroundColor='green';
+items[1].style.backgroundColor='green';
 for(var i=0;i<items.length;i++){
     items[i].style.fontWeight='bold';
 }
+var items= document.getElementsByClassName('list-group-item');
+items[4].style.backgroundColor='green';
+var li = document.getElementsByTagName('li');
+li[4].textContent='Hello 2';
+li[4].style.fontWeight='bold';
