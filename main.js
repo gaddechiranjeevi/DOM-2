0c36ba28d454d4f8c5c5bc69a08ef8085b407033
gaddechiranjeevi <gaddechiru@gmail.com>
2022-09-28 14:45:24 +0530

    query selector

--- main.js
+++ main.js
@@ -1,11 +1,6 @@
 
-var items= document.getElementsByClassName('list-group-item');
-items[1].style.backgroundColor='green';
-for(var i=0;i<items.length;i++){
-    items[i].style.fontWeight='bold';
-}
-var items= document.getElementsByClassName('list-group-item');
-items[4].style.backgroundColor='green';
-var li = document.getElementsByTagName('li');
-li[4].textContent='Hello 2';
-li[4].style.fontWeight='bold';
+var item= document.querySelector('.list-group-item:nth-child(3)');
+item.style.display='none';
+
+var secondItem = document.querySelector('.list-group-item:nth-child(2)');
+secondItem.style.backgroundColor = 'green';
